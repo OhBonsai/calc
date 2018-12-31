429bdd207122eb6440f9caa34b5b8219c6ba92cf
OhBonsai <letbonsaibe@gmail.com>
2019-01-01 00:32:04 +0800

    update

--- calc.go
+++ calc.go
@@ -1,6 +1,10 @@
 package calc
 
-import "github.com/OhBonsai/yolang/token"
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+)
 
 const (
 	ILLEGAL = "ILLEGAL"
@@ -9,7 +13,6 @@ const (
 
 	PLUS     = "+"
 	MINUS    = "-"
-	BANG     = "!"
 	ASTERISK = "*"
 	SLASH    = "/"
 
@@ -17,8 +20,28 @@ const (
 	RPAREN = ")"
 )
 
-func Calc(input string) int64 {
+const (
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > or <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X or !X
+	CALL        // myFunction(X)
+	INDEX       // array[index]
+)
 
+var precedences = map[string]int{
+	PLUS:     SUM,
+	MINUS:    SUM,
+	SLASH:    PRODUCT,
+	ASTERISK: PRODUCT,
+	LPAREN:   CALL,
+}
+
+func Calc(input string) int64 {
+	return 1
 }
 
 type Token struct {
@@ -95,8 +118,205 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
 func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
 	}
+}
+
+// ast
+
+type Expression interface {
+	String() string
+}
+
+type IntegerLiteralExpression struct {
+	Token Token
+	Value int64
+}
+
+func (il *IntegerLiteralExpression) String() string { return il.Token.Literal }
+
+type PrefixExpression struct {
+	Token    Token
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+type InfixExpression struct {
+	Token    Token
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Operator)
+	out.WriteString(" ")
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+// parser
+type (
+	prefixParseFn func() Expression
+	infixParseFn  func(Expression) Expression
+)
+
+type Parser struct {
+	l *Lexer
+
+	curToken  Token
+	peekToken Token
+
+	prefixParseFns map[string]prefixParseFn
+	infixParseFns  map[string]infixParseFn
+
+	errors []string
+}
+
+func (p *Parser) registerPrefix(tokenType string, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType string, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
+}
+
+func New(l *Lexer) *Parser {
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
+
+	p.prefixParseFns = make(map[string]prefixParseFn)
+	p.registerPrefix(INT, p.parseIntegerLiteral)
+	p.registerPrefix(MINUS, p.parsePrefixExpression)
+
+	p.infixParseFns = make(map[string]infixParseFn)
+	p.registerInfix(PLUS, p.parseInfixExpression)
+	p.registerInfix(MINUS, p.parseInfixExpression)
+	p.registerInfix(SLASH, p.parseInfixExpression)
+	p.registerInfix(ASTERISK, p.parseInfixExpression)
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) parseExpression(precedence int) Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+	leftExp := prefix()
+
+	for precedence < p.peekPrecedence() {
+		infix := p.infixParseFns[p.curToken.Type]
+		if infix == nil {
+			return leftExp
+		}
+
+		p.nextToken()
+		leftExp = infix(leftExp)
+	}
+
+	return leftExp
+}
+
+func (p *Parser) peekPrecedence() int {
+	if p, ok := precedences[p.peekToken.Type]; ok {
+		return p
+	}
+	return LOWEST
+}
+
+func (p *Parser) curPrecedence() int {
+	if p, ok := precedences[p.curToken.Type]; ok {
+		return p
+	}
+	return LOWEST
+}
+
+func (p *Parser) peekError(t string) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instend",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) parseIntegerLiteral() Expression {
+
+	lit := &IntegerLiteralExpression{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+	return lit
+}
+
+func (p *Parser) parsePrefixExpression() Expression {
+
+	expression := &PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+	p.nextToken()
+	expression.Right = p.parseExpression(PREFIX)
+	return expression
+}
+
+func (p *Parser) parseInfixExpression(left Expression) Expression {
+
+	expression := &InfixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+		Left:     left,
+	}
+
+	precedence := p.curPrecedence()
+	p.nextToken()
+
+	// // 通过降低优先级，来达到右结合
+	//if expression.Operator == "+" {
+	//	expression.Right = p.parseExpression(precedence - 1)
+	//} else {
+	//	expression.Right = p.parseExpression(precedence)
+	//}
+	expression.Right = p.parseExpression(precedence)
+
+	return expression
 }
